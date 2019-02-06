7871facf8ad1e86bcc892d93f5b0647c5d6e615a
Alessandro Morelli <amorelli@ihmc.us>
2019-02-06 14:06:14 -0600

    Added missing version.cpp file to compile NetProxy on Linux OS

---
+++ aci/cpp/netProxy/version.cpp
@@ -0,0 +1,8 @@
+#include "version.h"
+
+const char * _BUILD_DATE = __DATE__;
+const char * _BUILD_TIME = __TIME__;
+const char * _GIT_BRANCH = GIT_BRANCH;
+const char * _GIT_SHA = GIT_SHA;
+const char * _GIT_TAG = GIT_TAG;
+const char * _GIT_DATE = GIT_DATE;
