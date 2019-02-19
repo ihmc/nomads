b8ffc6bff5eb7c9af9cdbae6db5a4f60a36f6272
Alessandro Morelli <amorelli@ihmc.us>
2019-02-19 17:14:54 -0600

    Updated compile_proto scripts and removed unnecessary protos

--- misc/cpp/netsensor/proto/compile_proto.bat
+++ misc/cpp/netsensor/proto/compile_proto.bat
@@ -1,21 +1,43 @@
 @ECHO OFF
 
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && SET ARCH=x86 || SET ARCH=x64
+if %ARCH%==x64 (
+	echo "Detected 64-bit CPU architecture"
+) else (
+	echo "Detected 32-bit CPU architecture"
+)
+
 SET PROTO_VER=3.6.1
-SET PROTO_BIN=..\..\..\..\externals-win\protobuf\%PROTO_VER%\bin\protoc.exe
-SET PROTO_INCLUDE=..\..\..\..\externals-win\protobuf\%PROTO_VER%\include\
-SET SRC_DIR=..\proto\
-SET OUT_DIR=..\
+SET BIN=..\..\..\..\externals-win\protobuf\%PROTO_VER%\bin\%ARCH%\protoc.exe
+SET INCLUDE_DIR=..\..\..\..\externals-win\protobuf\%PROTO_VER%\include\%ARCH%
+SET SRC_DIR=..\..\..\..\..\measure
+SET OUT_DIR=..
+SET TMP_DIR=temp
 
+if not exist %TMP_DIR% mkdir %TMP_DIR%
+for %%f in (%SRC_DIR%\*.proto) do (
+	echo "Compiling %%f"
+	%BIN% --cpp_out=%TMP_DIR% -I%SRC_DIR% -I%INCLUDE_DIR% %%f
+)
+for %%f in (*.proto) do (
+	echo "Compiling %%f"
+	%BIN% --cpp_out=%TMP_DIR% -I. -I%INCLUDE_DIR% %%f
+)
 
-%PROTO_BIN% --cpp_out=%OUT_DIR% container.proto -I=%PROTO_INCLUDE% -I%SRC_DIR%
-%PROTO_BIN% --cpp_out=%OUT_DIR% datatype.proto -I=%PROTO_INCLUDE% -I%SRC_DIR%
-%PROTO_BIN% --cpp_out=%OUT_DIR% icmpinfo.proto -I=%PROTO_INCLUDE% -I%SRC_DIR%
-%PROTO_BIN% --cpp_out=%OUT_DIR% iw.proto -I=%PROTO_INCLUDE% -I%SRC_DIR%
-%PROTO_BIN% --cpp_out=%OUT_DIR% measure.proto -I=%PROTO_INCLUDE% -I%SRC_DIR%
-%PROTO_BIN% --cpp_out=%OUT_DIR% netproxyinfo.proto -I=%PROTO_INCLUDE% -I%SRC_DIR%
-%PROTO_BIN% --cpp_out=%OUT_DIR% prefix.proto -I=%PROTO_INCLUDE% -I%SRC_DIR%
-%PROTO_BIN% --cpp_out=%OUT_DIR% rtt.proto -I=%PROTO_INCLUDE% -I%SRC_DIR%
-%PROTO_BIN% --cpp_out=%OUT_DIR% subject.proto -I=%PROTO_INCLUDE% -I%SRC_DIR%
-%PROTO_BIN% --cpp_out=%OUT_DIR% topology.proto -I=%PROTO_INCLUDE% -I%SRC_DIR%
-%PROTO_BIN% --cpp_out=%OUT_DIR% container.proto -I=%PROTO_INCLUDE% -I%SRC_DIR%
-%PROTO_BIN% --cpp_out=%OUT_DIR% traffic.proto -I=%PROTO_INCLUDE% -I%SRC_DIR%
+for %%f in (%TMP_DIR%/*) do (
+	if not exist %OUT_DIR%/%%f (
+		echo "Copying %TMP_DIR%\%%f into %OUT_DIR%\%%f"
+		xcopy "%TMP_DIR%\%%f" "%OUT_DIR%"
+	) else (
+		fc "%TMP_DIR%\%%f" "%OUT_DIR%\%%f" > nul
+		if errorlevel 1 (
+			echo "Copying %TMP_DIR%\%%f into %OUT_DIR%\%%f"
+			xcopy "%TMP_DIR%\%%f" "%OUT_DIR%\" /Y
+		) else (
+			echo "%OUT_DIR%\%%f already up-to-date; nothing to do."
+		)
+	)
+)
+
+echo "Cleaning temp folder..."
+@RD /S /Q %TMP_DIR%
---
+++ misc/cpp/netsensor/proto/compile_proto.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+PROTO_VER=3.1.0
+ARCH=`sh ../../../../util/scripts/guessArch.sh`
+
+BIN=../../../../externals/protobuf/protobuf-${PROTO_VER}/${ARCH}/protoc/bin/protoc
+SRC_DIR=../../../../../measure
+OUT_DIR=..
+TMP_DIR=temp
+
+# Create temp dir
+if [ ! -e "${TMP_DIR}" ]
+then
+    echo "Creating temporary directory: `pwd`/$TMP_DIR"
+    mkdir "$TMP_DIR"
+fi
+
+# Compile protobuf files
+for f in `ls ${SRC_DIR}/*.proto 2> /dev/null`
+do
+    echo "Compiling ${f}"
+    eval "${BIN} --cpp_out=${TMP_DIR} -I${SRC_DIR} ${f}"
+done
+
+# Overwrite old files if they differ
+for f in `ls ${TMP_DIR}/* 2> /dev/null`
+do
+    FILE_NAME=$(basename $f)
+    if ! `cmp --silent "${TMP_DIR}/${FILE_NAME}" "${OUT_DIR}/${FILE_NAME}"`
+    then
+        cp "${TMP_DIR}/${FILE_NAME}" "${OUT_DIR}/${FILE_NAME}"
+        echo "Updated ${FILE_NAME}"
+    fi
+done
+
+#Cleanup
+echo "Removing temporary directory: `pwd`/${TMP_DIR}"
+rm -rf ${TMP_DIR}
