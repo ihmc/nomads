ca94190d228326fb873c53c70738e401e0bd891e
Alessandro Morelli <amorelli@ihmc.us>
2019-02-19 17:13:51 -0600

    Removed unnecessary protos and updated compile_proto.bat

--- aci/cpp/netProxy/proto/compile_proto.bat
+++ aci/cpp/netProxy/proto/compile_proto.bat
@@ -1,7 +1,15 @@
 @ECHO OFF
 
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && SET ARCH=x86 || SET ARCH=x64
+if %ARCH%==x64 (
+	echo "Detected 64-bit CPU architecture"
+) else (
+	echo "Detected 32-bit CPU architecture"
+)
+
 SET PROTO_VER=3.6.1
-SET BIN=..\..\..\..\externals-win\protobuf\%PROTO_VER%\bin\protoc.exe
+SET BIN=..\..\..\..\externals-win\protobuf\%PROTO_VER%\bin\%ARCH%\protoc.exe
+SET INCLUDE_DIR=..\..\..\..\externals-win\protobuf\%PROTO_VER%\include\%ARCH%
 SET SRC_DIR=..\..\..\..\..\measure
 SET OUT_DIR=..
 SET TMP_DIR=temp
@@ -9,7 +17,7 @@ SET TMP_DIR=temp
 if not exist %TMP_DIR% mkdir %TMP_DIR%
 for %%f in (%SRC_DIR%\*.proto) do (
 	echo "Compiling %%f"
-	%BIN% --cpp_out=%TMP_DIR% -I%SRC_DIR% %%f
+	%BIN% --cpp_out=%TMP_DIR% -I%SRC_DIR% -I%INCLUDE_DIR% %%f
 )
 
 for %%f in (%TMP_DIR%/*) do (
